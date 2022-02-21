bc21f32f6ffbcfefaa4951e6939da60ea812de6f
tupikov.dm <Tupikov.Dm@citilink.ru>
2022-02-21 12:16:02 +0300

    sprint2/step2 Удален неиспользуемый useSelect в Modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -4,17 +4,9 @@ import PropTypes from 'prop-types';
 import styles from './modal.module.css';
 import close from '../../images/close.svg';
 import ModalOverlay from '../modal-overlay/modal-overlay';
-import { useSelector } from 'react-redux';
 
 
 function Modal({onClose, children}) {
-  const { itemObject } = useSelector(
-    state => state.ingredients
-  );
-  const { orderObject } = useSelector(
-    state => state.order
-  );
-
   const modalRoot = document.getElementById("react-modals");
 
   const onPressEsc = React.useCallback((e) => {
@@ -29,7 +21,7 @@ function Modal({onClose, children}) {
   }, [onPressEsc]);
 
   return ReactDOM.createPortal(
-  	<div className={((itemObject != null) || (orderObject != null)) ? styles.wrap_active : styles.wrap}>
+  	<div className={(children != null) ? styles.wrap_active : styles.wrap}>
       <ModalOverlay onClose={onClose} />
       <div className={styles.modal}>
         {children}
