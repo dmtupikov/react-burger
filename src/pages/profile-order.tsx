a0f49e76be505dd838fd28f0cd4b813f8178aff1
tupikov.dm <Tupikov.Dm@citilink.ru>
2022-04-17 19:59:58 +0300

    sprint6/step1 устранены недочеты2

--- src/pages/profile-order.tsx
+++ src/pages/profile-order.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, FC } from 'react';
 import { useDispatch } from '../services/hooks';
-import { WS_CONNECTION_START_USER, WS_CONNECTION_CLOSED_USER } from '../services/actions/wsActions';
+import { WS_CONNECTION_START_USER, WS_CONNECTION_CLOSED_USER } from '../services/actions/wsActionsUser';
 import FeedDetails from '../components/feed-details/feed-details';
 
 
--- src/pages/profile-orders.tsx
+++ src/pages/profile-orders.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, FC } from 'react';
 import { useDispatch, useSelector } from '../services/hooks';
-import { WS_CONNECTION_START_USER, WS_CONNECTION_CLOSED_USER } from '../services/actions/wsActions';
+import { WS_CONNECTION_START_USER, WS_CONNECTION_CLOSED_USER } from '../services/actions/wsActionsUser';
 import ProfileMenu from '../components/profile-menu/profile-menu';
 import FeedItem from '../components/feed/feed-item';
 import styles from './profile.module.css';
